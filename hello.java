8a5dcc6d12859014b96ff0c2a3e86519f3e338aa
Tiến <72064382+hoangtien18087131@users.noreply.github.com>
2021-09-08 16:59:10 +0700

    Create hello.java
    Demo file hello.java

---
+++ hello.java
@@ -0,0 +1,7 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Teacher!");
+        System.out.println("My name is Luu Hoang Tien");
+        System.out.println("Class DHCNTT14");
+    }
+}
