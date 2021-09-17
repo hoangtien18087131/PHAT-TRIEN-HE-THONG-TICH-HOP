9205ce16f49a9f4935bbd8c8070a026a324d0d4e
Tiến <72064382+hoangtien18087131@users.noreply.github.com>
2021-09-17 20:24:23 +0700

    Add files via upload

---
+++ Cau3.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Cau3 {
+    public static void giaiPTBac2(float a, float b, float c) {
+        if (a == 0) {
+            if (b == 0) {
+                System.out.println("Phương trình vô nghiệm!");
+            } else {
+                System.out.println("Phương trình có một nghiệm: "+ "x = " + (-c / b));
+            }
+            return;
+        }
+        float delta = b*b - 4*a*c;
+        float x1;
+        float x2;
+        if (delta > 0) {
+            x1 = (float) ((-b + Math.sqrt(delta)) / (2*a));
+            x2 = (float) ((-b - Math.sqrt(delta)) / (2*a));
+            System.out.println("Phương trình có 2 nghiệm là: "+ "x1 = " + x1 + " và x2 = " + x2);
+        } else if (delta == 0) {
+            x1 = (-b / (2 * a));
+            System.out.println("Phương trình có nghiệm kép: " + "x1 = x2 = " + x1);
+        } else {
+            System.out.println("Phương trình vô nghiệm!");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Nhập a: ");
+        int a = scanner.nextInt();
+        System.out.println("Nhập b: ");
+        int b = scanner.nextInt();
+        System.out.println("Nhập c: ");
+        int c = scanner.nextInt();
+        giaiPTBac2(a, b, c);
+    }
+
+}
